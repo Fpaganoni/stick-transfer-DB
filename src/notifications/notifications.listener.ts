8b370fa97e8fc210248f9587ff1902883adfa190
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2026-05-19 22:22:34 +0200

    refactor: remove social event listeners from notifications service
    - Remove post.liked, post.commented, comment.replied, comment.liked, and user.followed event listeners
    - Remove postId field from NotificationEvent interface

--- src/notifications/notifications.listener.ts
+++ src/notifications/notifications.listener.ts
@@ -8,50 +8,14 @@ export interface NotificationEvent {
   recipientId: string;
   type: NotificationType;
   entityId?: string;
-  postId?: string;
 }
 
 @Injectable()
 export class NotificationsListener {
   constructor(private notificationsService: NotificationsService) {}
 
-  @OnEvent('post.liked')
-  async handlePostLiked(event: NotificationEvent) {
-    try {
-      await this.notificationsService.createNotification(event);
-    } catch (_) {}
-  }
-
-  @OnEvent('post.commented')
-  async handlePostCommented(event: NotificationEvent) {
-    try {
-      await this.notificationsService.createNotification(event);
-    } catch (_) {}
-  }
-
-  @OnEvent('comment.replied')
-  async handleCommentReplied(event: NotificationEvent) {
-    try {
-      await this.notificationsService.createNotification(event);
-    } catch (_) {}
-  }
-
-  @OnEvent('comment.liked')
-  async handleCommentLiked(event: NotificationEvent) {
-    try {
-      await this.notificationsService.createNotification(event);
-    } catch (_) {}
-  }
-
   @OnEvent('club.invite_sent')
   async handleClubInvite(event: NotificationEvent) {
-    try {
-      await this.notificationsService.createNotification(event);
-    } catch (_) {}
-  }
-
-  @OnEvent('user.followed')
-  async handleUserFollowed(event: NotificationEvent) {
     try {
       await this.notificationsService.createNotification(event);
     } catch (_) {}
